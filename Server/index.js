fb1f5905a5f6b25c847b2e35ff8dd9ebce8c3c7b
chakriswamireddy <iamthechakri@gmail.com>
2023-10-27 17:32:44 +0530

    adding server

--- Server/index.js
+++ Server/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 const rawData = fs.readFileSync('db.json');
 const data = JSON.parse(rawData);
